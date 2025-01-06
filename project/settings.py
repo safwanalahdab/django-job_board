aa0083d85464513137916bfd632e2f5f4a68b151
safwan alahdab <safwan.alahdb@gmail.com>
2025-01-06 22:06:30 +0300

    static

--- project/settings.py
+++ project/settings.py
@@ -118,8 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
