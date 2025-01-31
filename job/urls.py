46143b7fdac8c0810f3fcc91a234b1b466971855
safwan alahdab <safwan.alahdb@gmail.com>
2025-01-31 14:13:50 +0300

    frontend

--- job/urls.py
+++ job/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path , include 
 from . import views
 from .views import *
-  
+app_name = "job" 
+
 urlpatterns = [
     path('' , views.job_list ) ,
-    path('<int:id>' , views.job_detail )
+    path('<int:id>' , views.job_detail , name = "job_detail" )
 ]
--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('job/' , include('job.urls')) , 
+    path('job/' , include('job.urls',namespace="job/") ) , 
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
