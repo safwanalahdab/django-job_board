980a41354b4d88051a381f5938008c9786498a5c
safwan alahdab <safwan.alahdb@gmail.com>
2025-06-03 19:28:49 +0300

    edit profile

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login
 from .forms import Signup 
 from .models import*
 from .models import Profile 
+from .forms import UserForm , ProfileForm 
+from django.urls import reverse 
+
 
 # Create your views here.
 
@@ -29,5 +32,19 @@ def profile ( request ) :
    return render( request ,"accounts/profile.html",{ 'profile' : profile })
 
 def profile_edit ( request ) :
+   profile = Profile.objects.get( user = request.user )
    
-   return render( request , "accounts/profile_edit.html" , {} ) 
+   if request.method == 'POST' :
+      userform = UserForm( request.POST , instance = request.user ) 
+      profileform = ProfileForm( request.POST , request.FILES , instance = profile )
+      if userform.is_valid() and profileform.is_valid() :
+         userform.save() 
+         x = profileform.save( commit = False )
+         x.user = request.user 
+         x.save() 
+         return redirect( reverse('accounts:profile') )
+   else : 
+      userform = UserForm( instance = request.user ) 
+      profileform = ProfileForm( instance = profile ) 
+   
+   return render( request , "accounts/profile_edit.html" , { 'userform' : userform , 'profileform' : profileform } ) 
